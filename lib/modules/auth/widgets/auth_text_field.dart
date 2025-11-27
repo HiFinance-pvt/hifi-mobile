3d1301c83f16d8d13a074e90a78cfb9f2226e33c
Abhimanyu0403 <pranav.sonawane019@gmail.com>
2025-11-28 00:28:27 +0530

    feat: update auth widgets to match Figma design

--- lib/modules/auth/widgets/auth_text_field.dart
+++ lib/modules/auth/widgets/auth_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hifi/shared/themes/app_theme.dart';
 
 class AuthTextField extends StatelessWidget {
   final TextEditingController controller;
@@ -20,14 +21,38 @@ class AuthTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      validator: validator,
-      keyboardType: keyboardType,
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        labelText: label,
-        suffixIcon: suffixIcon,
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(14),
+        border: Border.all(color: Colors.white),
+      ),
+      child: TextFormField(
+        controller: controller,
+        validator: validator,
+        keyboardType: keyboardType,
+        obscureText: obscureText,
+        style: const TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: AppTheme.gray900,
+          height: 1.5,
+        ),
+        decoration: InputDecoration(
+          hintText: label,
+          hintStyle: const TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: AppTheme.gray600,
+            height: 1.5,
+          ),
+          suffixIcon: suffixIcon,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+        ),
       ),
     );
   }
